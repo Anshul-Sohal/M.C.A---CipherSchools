769ba433a1c221bf70799cf60ed63a306a7421e7
Anshul Sohal <anshulsohal.15@gmail.com>
2024-06-12 09:37:36 +0530

    function

---
+++ 05_Functions/function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int add(int a,int b){
+    return a+b;
+}
+int main() {
+    int a,b;
+    cout<<"Enter a number=";
+    cin>>a;
+    cout<<"Enter another number=";
+    cin>>b;
+    return 0;
+}
