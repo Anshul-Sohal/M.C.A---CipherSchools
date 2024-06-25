2e6cf043b2177c89363de8241d57c5a04554d03a
Anshul Sohal <anshulsohal.15@gmail.com>
2024-06-25 11:28:03 +0530

    Sorting array

--- 10_DSA/01_Array's/Sorting/Bubble Sort.cpp
+++ 10_DSA/01_Array's/Sorting/Bubble Sort.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 using namespace std;
 
+// void bubbleSort(int arr[],int size){
+//     for(int i = size-1;i>=0;i--){
+//         for(int j = 0;j<i;j++){
+//             if(arr[j]>arr[j+1]){
+//                 swap(arr[j],arr[j+1]);
+//             }
+//         }
+//     }
+// }
+//Optimized version
 void bubbleSort(int arr[],int size){
-    for(int i = size-1;i>=0;i--){
-        for(int j = 0;j<i;j++){
+    for(int i = size-1; i>=0; i--){
+        int hasSwapped = 0;
+        for(int j = 0; j<i; j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                hasSwapped = 1;
             }
+        }
+        if(hasSwapped == 0){
+            break;
         }
     }
 }
-
 int main() {
     int size;
     cout<<"Enter the size of the array";
